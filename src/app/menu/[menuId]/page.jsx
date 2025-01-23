749b9981852c380f42a84d8bc6f4077115e35b6d
fasfashann <saintgloria.jkt@gmail.com>
2025-01-23 15:28:44 +0700

    add back to menu link

--- src/app/menu/[menuId]/page.jsx
+++ src/app/menu/[menuId]/page.jsx
@@ -51,6 +51,26 @@ export default function MenuDetail() {
 
   return (
     <div className="max-w-5xl mx-auto p-4 mt-10">
+      <Link
+        href="/"
+        className="text-primary font-semibold hover:underline flex items-center gap-2  mb-10"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M11.0303 3.96967C11.3232 4.26256 11.3232 4.73744 11.0303 5.03033L4.81066 11.25H21C21.4142 11.25 21.75 11.5858 21.75 12C21.75 12.4142 21.4142 12.75 21 12.75H4.81066L11.0303 18.9697C11.3232 19.2626 11.3232 19.7374 11.0303 20.0303C10.7374 20.3232 10.2626 20.3232 9.96967 20.0303L2.46967 12.5303C2.17678 12.2374 2.17678 11.7626 2.46967 11.4697L9.96967 3.96967C10.2626 3.67678 10.7374 3.67678 11.0303 3.96967Z"
+            fill="#C12126"
+          />
+        </svg>
+        Back to menu
+      </Link>
       <div className="grid md:grid-cols-12 gap-8">
         <div className="col-span-5">
           <img
